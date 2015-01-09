ffc78005013f6584da270769fb1f783112a4ae6a
Victor Chen <vyc4@duke.edu>
2015-01-09 03:02:15 -0500

    Plan

---
+++ README.txt
@@ -0,0 +1,25 @@
+Worked with: 
+Date Started: 1/8/15
+
+Number of Hours: 
+
+Resources Used:
+http://docs.oracle.com/javafx/2/get_started/hello_world.htm
+CPS308 Spring 2015 Webpage
+
+Files used to start:
+
+
+Files used to test:
+
+
+Data/resources required:
+
+
+Program use:
+
+Bugs/Crashes/Problems:
+
+Extra Features:
+
+Impressions:
---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,39 @@
+/**
+ *  Copied from http://docs.oracle.com/javafx/2/get_started/hello_world.html
+ *  This class is a test to verify operation of JavaFX on vyc4's VAIO. 
+ */
+
+package helloworld;
+ 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class HelloWorld extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
